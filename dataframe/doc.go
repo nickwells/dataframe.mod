172b5150716b6dd2aba6e1ffdbc0edcfc2257d53
Nick Wells <wells.nicholas@gmail.com>
2020-09-21 11:56:54 +0100

    Add package-level documentation

---
+++ dataframe/doc.go
@@ -0,0 +1,8 @@
+/*
+
+Package dataframe provides a structure for holding a grid of data called a
+dataframe. It reflects a series of columns of different data types each with
+the same number of rows.
+
+*/
+package dataframe
